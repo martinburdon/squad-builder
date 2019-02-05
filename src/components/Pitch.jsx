596e9fb72df2b9c3d281fb6bce08c4b506dff9a3
Martin Burdon <martin@martinburdon.co.uk>
2019-02-05 20:07:56 +0000

    Move pitch styles to component

--- src/components/Pitch.jsx
+++ src/components/Pitch.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Player from './Player.jsx';
+import styled from 'styled-components';
+
+const Pitch = styled.div`
+  background: mediumseagreen;
+  height: 50rem;
+  position: relative;
+  width: 40rem;
+`;
 
 export default ({ formation, players }) => {
   const data = players.map((player, x) => <Player {...player} key={x} />);
   return (
-    <div className="pitch">
+    <Pitch>
       {data}
-    </div>
+    </Pitch>
   )
 };
